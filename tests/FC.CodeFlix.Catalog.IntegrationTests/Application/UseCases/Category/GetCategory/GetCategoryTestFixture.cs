f2af0c55f9bdeda54cbe6f4d593131605cca17e8
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2023-10-12 20:54:13 -0700

    Added GetCategoryTest integration;

---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollection : ICollectionFixture<GetCategoryTestFixture> { }
+
+public class GetCategoryTestFixture : CategoryUseCaseBaseFixture
+{
+
+}
