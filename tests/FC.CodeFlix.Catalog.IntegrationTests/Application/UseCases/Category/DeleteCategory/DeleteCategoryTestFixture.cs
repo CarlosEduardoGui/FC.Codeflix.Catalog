c965aef711f4035abf2e1d8d5e0180f28906f8ff
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2023-10-18 22:12:43 -0700

    Added DeleteCategory Integration Test;

---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }
+
+public class DeleteCategoryTestFixture : CategoryUseCaseBaseFixture
+{
+}
