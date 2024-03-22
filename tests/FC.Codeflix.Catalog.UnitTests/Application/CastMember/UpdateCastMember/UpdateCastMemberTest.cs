f360ab19d8d94db2e1c76cd1fa277089a574cd03
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2024-03-21 21:03:34 -0700

    test: add unit test for update cast member

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/UpdateCastMember/UpdateCastMemberTest.cs
@@ -0,0 +1,64 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Repository;
+using Moq;
+using Xunit;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.CastMember.UpdateCastMember;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Application.UseCases.CastMember.UpdateCastMember;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CastMember.UpdateCastMember;
+
+[Collection(nameof(UpdateCastMemberTestFixture))]
+public class UpdateCastMemberTest
+{
+    private readonly UpdateCastMemberTestFixture _fixture;
+
+    public UpdateCastMemberTest(UpdateCastMemberTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Trait("Use Cases", "UpdateCastMember - Use Cases")]
+    [Fact(DisplayName = nameof(Update))]
+    public async Task Update()
+    {
+        var repositoryMock = new Mock<ICastMemberRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var castMemberExample = _fixture.GetExampleCastMember();
+        var newName = _fixture.GetValidName();
+        var newType = _fixture.GetRandomCastMemberType();
+        var input = new UpdateCastMemberInput(
+            castMemberExample.Id,
+            newName,
+            newType
+        );
+        repositoryMock.Setup(x => x.GetByIdAsync(
+                It.Is<Guid>(x => x == castMemberExample.Id),
+                It.IsAny<CancellationToken>()
+            )
+        ).ReturnsAsync(castMemberExample);
+        var useCase = new UseCase.UpdateCastMember(repositoryMock.Object, unitOfWorkMock.Object);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(input.Id);
+        output.Name.Should().Be(input.Name);
+        output.Type.Should().Be(input.Type);
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+        unitOfWorkMock.Verify(x =>
+            x.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+        repositoryMock.Verify(x => x.GetByIdAsync(
+                It.Is<Guid>(x => x == input.Id), 
+                It.IsAny<CancellationToken>()
+            ), Times.Once
+        );
+        repositoryMock.Verify(x => x.UpdateAsync(
+                It.Is<DomainEntity.CastMember>(
+                    x => x.Name == input.Name 
+                    && x.Type == input.Type
+                ),
+                It.IsAny<CancellationToken>()
+            ), Times.Once
+        );
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/UpdateCastMember/UpdateCastMemberTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Application.CastMember.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CastMember.UpdateCastMember;
+
+[CollectionDefinition(nameof(UpdateCastMemberTestFixture))]
+public class UpdateCastMemberTestFixtureCollection : ICollectionFixture<UpdateCastMemberTestFixture> { }
+
+public class UpdateCastMemberTestFixture : CastMemberUseCaseBaseFixture
+{
+}
