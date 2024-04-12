3dee2c5cb130704e4ef3b5dd89608349c595123b
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2024-04-11 19:14:17 -0700

    test: add integration test for use case get cast member

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/CastMember/GetCastMember/GetCastMemberTest.cs
@@ -0,0 +1,60 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.CastMember.Common;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.CastMember.GetCastMember;
+using Xunit;
+using FC.Codeflix.Catalog.Application.UseCases.CastMember.GetCastMember;
+using FluentAssertions;
+using FC.Codeflix.Catalog.Application.Exceptions;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.CastMember.GetCastMember;
+
+[Collection(nameof(CastMemberUseCaseBaseFixture))]
+public class GetCastMemberTest
+{
+    private readonly CastMemberUseCaseBaseFixture _fixture;
+
+    public GetCastMemberTest(CastMemberUseCaseBaseFixture fixture) 
+        => _fixture = fixture;
+
+    [Trait("Integration/Application", "GetCastMember - Use Cases")]
+    [Fact(DisplayName = nameof(GetCastMember))]
+    public async Task GetCastMember()
+    {
+        var castMemberExamples = _fixture.GetExampleCastMembersList();
+        var exampleTaget = castMemberExamples[5];
+        var arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.AddRangeAsync(castMemberExamples);
+        await arrangeDbContext.SaveChangesAsync();
+        var repository = _fixture.CastMemberRepository(arrangeDbContext);
+        var useCase = new UseCase.GetCastMember(repository);
+        var input = new GetCastMemberInput(exampleTaget.Id);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(exampleTaget.Id);
+        output.Name.Should().Be(exampleTaget.Name);
+        output.Type.Should().Be(exampleTaget.Type);
+        output.CreatedAt.Should().BeSameDateAs(exampleTaget.CreatedAt);
+    }
+
+    [Trait("Integration/Application", "GetCastMember - Use Cases")]
+    [Fact(DisplayName = nameof(GetCastMemberThrowsWhenNotFound))]
+    public async Task GetCastMemberThrowsWhenNotFound()
+    {
+        var castMemberExamples = _fixture.GetExampleCastMembersList();
+        var exampleTaget = Guid.NewGuid();
+        var arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.AddRangeAsync(castMemberExamples);
+        await arrangeDbContext.SaveChangesAsync();
+        var repository = _fixture.CastMemberRepository(arrangeDbContext);
+        var useCase = new UseCase.GetCastMember(repository);
+        var input = new GetCastMemberInput(exampleTaget);
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await action
+            .Should()
+            .ThrowExactlyAsync<NotFoundException>()
+            .WithMessage($"CastMember '{exampleTaget}' not found.");
+    }
+}
