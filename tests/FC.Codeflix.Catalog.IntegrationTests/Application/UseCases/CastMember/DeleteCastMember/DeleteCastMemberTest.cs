349a414fdb5159111e41046c99532155ff0918f6
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2024-04-11 19:26:12 -0700

    test: add integration test for use case delete cast member

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
@@ -0,0 +1,63 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.CastMember.Common;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.CastMember.DeleteCastMember;
+using Xunit;
+using UoW = FC.Codeflix.Catalog.Infra.Data.EF.UnitOfWork.UnitOfWork;
+using FC.Codeflix.Catalog.Application.UseCases.CastMember.DeleteCastMember;
+using Microsoft.EntityFrameworkCore;
+using FluentAssertions;
+using FC.Codeflix.Catalog.Application.Exceptions;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.CastMember.DeleteCastMember;
+
+[Collection(nameof(CastMemberUseCaseBaseFixture))]
+public class DeleteCastMemberTest
+{
+    private readonly CastMemberUseCaseBaseFixture _fixture;
+
+    public DeleteCastMemberTest(CastMemberUseCaseBaseFixture fixture) 
+        => _fixture = fixture;
+
+    [Trait("Integration/Application", "DeleteCastMember - Use Cases")]
+    [Fact(DisplayName = nameof(Delete))]
+    public async Task Delete()
+    {
+        var example = _fixture.GetExampleCastMember();
+        var arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.AddRangeAsync(example);
+        await arrangeDbContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var repository = _fixture.CastMemberRepository(actDbContext);
+        var unitOfWork = new UoW(actDbContext);
+        var useCase = new UseCase.DeleteCastMember(repository, unitOfWork);
+        var input = new DeleteCastMemberInput(example.Id);
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var list = await assertDbContext.CastMembers.AsNoTracking().ToListAsync();
+        list.Should().HaveCount(0);
+    }
+
+    [Trait("Integration/Application", "DeleteCastMember - Use Cases")]
+    [Fact(DisplayName = nameof(DeleteThrowsWhenNotFound))]
+    public async Task DeleteThrowsWhenNotFound()
+    {
+        var example = _fixture.GetExampleCastMember();
+        var arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.AddRangeAsync(example);
+        await arrangeDbContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var repository = _fixture.CastMemberRepository(actDbContext);
+        var unitOfWork = new UoW(actDbContext);
+        var useCase = new UseCase.DeleteCastMember(repository, unitOfWork);
+        var randomId = Guid.NewGuid();
+        var input = new DeleteCastMemberInput(randomId);
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await action
+            .Should()
+            .ThrowExactlyAsync<NotFoundException>()
+            .WithMessage($"CastMember '{randomId}' not found.");
+    }
+}
