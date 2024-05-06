d399a431d22cb8fed4943925275b607e93fd09f5
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2024-05-05 20:11:15 -0700

    test: add unit test for image value object

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/ValueObject/ImageTest.cs
@@ -0,0 +1,48 @@
+﻿using FC.Codeflix.Catalog.Domain.ValueObject;
+using FC.Codeflix.Catalog.UnitTests.Commom;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.ValueObject;
+public class ImageTest : BaseFixture
+{
+
+    [Trait("Domain", "Image - ValueObject")]
+    [Fact(DisplayName = nameof(Instantiate))]
+    public void Instantiate()
+    {
+        var path = Faker.Image.PicsumUrl();
+
+        var image = new Image(path);
+
+        image.Should().NotBeNull();
+        image.Path.Should().Be(path);
+    }
+
+    [Trait("Domain", "Image - ValueObject")]
+    [Fact(DisplayName = nameof(EqualsByProperts))]
+    public void EqualsByProperts()
+    {
+        var path = Faker.Image.PicsumUrl();
+        var image = new Image(path);
+        var sameImage = new Image(path);
+
+        var isItEquals = image == sameImage;
+
+        isItEquals.Should().BeTrue();
+    }
+
+    [Trait("Domain", "Image - ValueObject")]
+    [Fact(DisplayName = nameof(DifferentByProperts))]
+    public void DifferentByProperts()
+    {
+        var pathOne = Faker.Image.PicsumUrl();
+        var pathTwo = Faker.Image.PicsumUrl();
+        var image = new Image(pathOne);
+        var differentImage = new Image(pathTwo);
+
+        var isItEquals = image == differentImage;
+
+        isItEquals.Should().BeFalse();
+    }
+}
